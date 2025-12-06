a5b222fb9255701c189d0feceb030514a3da2d6d
SeifeddineTalks <seifeddinetalks@gmail.com>
2025-12-07 00:20:32 +0100

    kyu8:Square(n) Sum

---
+++ src/main/java/kata/kyu8/SquareSum.java
@@ -0,0 +1,21 @@
+package kata.kyu8;
+
+public class SquareSum {
+
+    public static int squareSum(int[] n)
+    {
+        int result=0;
+
+        for (int i=0; i<n.length; i++) {
+             result+=n[i]*n[i];
+        }
+        return result;
+    }
+
+
+    static void main(String[] args) {
+
+        System.out.println("Test 1 {1,2,2}: " + squareSum(new int[] {1,2,2}));
+    }
+
+}
