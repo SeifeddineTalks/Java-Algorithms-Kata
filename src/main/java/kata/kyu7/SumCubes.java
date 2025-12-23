444d9780039846c30717a742ee63b7a188603f9b
SeifeddineTalks <seifeddinetalks@gmail.com>
2025-12-24 00:33:30 +0100

    Kyu7: Sum of Cubes

---
+++ src/main/java/kata/kyu7/SumCubes.java
@@ -0,0 +1,21 @@
+package kata.kyu7;
+
+public class SumCubes {
+
+    public static long sumCubes (long n) {
+        long result = 0;
+
+        for (int i=1; i<=n; i++){
+            result+= (long) Math.pow(i,3);
+        }
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Input {2, 5, 34, 6} result: " + sumCubes (3));
+    }
+}
+
